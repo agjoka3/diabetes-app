ef6f28fbb42b6ce13ffcf8939f5c632573ae3078
agdev <agdev@Armelas-MacBook-Pro.local>
2022-12-06 09:20:39 +0100

    add auth in acivity

--- src/routes/activity/index.tsx
+++ src/routes/activity/index.tsx
@@ -1,8 +1,17 @@
 import { component$ } from "@builder.io/qwik";
-import { DocumentHead } from "@builder.io/qwik-city";
+import { DocumentHead, useNavigate } from "@builder.io/qwik-city";
+import { onAuthStateChanged } from "firebase/auth";
 import { Activities } from "~/components/activities/activities";
+import { auth } from "~/firebase";
 
 export default component$(() => {
+  const nav = useNavigate();
+  onAuthStateChanged(auth, ((user) => {
+    if (!user) {
+        nav.path = '/login'
+    }
+  })); 
+  
   return (
     <div>
       <h3>Welcome to Activity Module!</h3>
